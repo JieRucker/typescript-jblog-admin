5e8f095e32f76a3032daa6fde46f1c7e5fb5d987
jrucker <jrucker@163.com>
2019-02-13 00:37:16 +0800

    [fix] update add cross origin

--- build/server.js
+++ build/server.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const cors = require('./crossOrigin');
-
+// const cors = require('./crossOrigin');
+//
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
-app.use(cors({
+/*app.use(cors({
   origin: function (ctx) {
     if (ctx.url === '/test') {
       return false;
@@ -18,7 +18,7 @@ app.use(cors({
   credentials: true,
   allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-}));
+}));*/
 
 app.get('*', function (req, res) {
   const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
