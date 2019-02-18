bf7f6ef8d52ab1fc265cccb4f56660160558e9d0
JieRucker <1435255141@qq.com>
2019-02-18 15:21:04 +0800

    [fix] update

--- src/api/server.js
+++ src/api/server.js
@@ -2,7 +2,6 @@
  * Created by JRucker on 2018/3/18.
  * api 接口
  */
-
 import loginInterface from './LoginInterface';
 import mainInterface from './MainInterface';
 import articleInterface from './ArticleInterface';
